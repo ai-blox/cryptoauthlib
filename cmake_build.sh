0fabb5e996101e77f2206432b06d66133b9a57be
Pedro Zorro <peter@ai-blox.com>
2023-05-22 12:41:37 +0000

    Add checkout correct version

--- cmake_build.sh
+++ cmake_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git checkout v3.3.3
 mkdir dbuild
 cd dbuild
 cmake -DATCA_PKCS11:STRING=ON -DATCA_HAL_I2C=ON -DATCA_PKCS11=ON -DATCA_OPENSSL=ON -DATCA_TNGTLS_SUPPORT=ON -DBUILD_TESTS=ON ..
